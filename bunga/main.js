186a8aea2d8147014226d3238ee677c9b8b1334f
izzyhost <126366045+izzyhost@users.noreply.github.com>
2024-10-29 21:13:26 +0700

    Add files via upload

---
+++ bunga/main.js
@@ -0,0 +1,18 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+function createFallingStar() {
+    const star = document.createElement('div');
+    star.classList.add('falling-star');
+    star.style.left = Math.random() * window.innerWidth + 'px';
+    star.style.animationDuration = Math.random() * 2 + 3 + 's';
+
+    document.body.appendChild(star);
+
+    star.addEventListener('animationend', () => {
+        star.remove();
+    });
+}
+
+setInterval(createFallingStar, 300);
